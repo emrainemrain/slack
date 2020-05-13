a8964b08d99cc141cdca97e227ddcca336cdb7c2
emrainemrain <emrainemrain@gmail.com>
2020-05-13 15:57:51 +0100

    update createChannel.java

--- src/day_1/createChannel.java
+++ src/day_1/createChannel.java
@@ -2,8 +2,12 @@ package day_1;
 
 public class createChannel {
     public static void main(String[] args) {
-
         System.out.println("user is able to create channels");
+
+        int x=0;//from interlij
+        if (x==0){
+            System.out.println("equal");
+        }
 
     }
 }
