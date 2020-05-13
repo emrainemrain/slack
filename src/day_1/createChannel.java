be2594aa580f87a4e8ccd7770fb04b13f435e9ba
emrainemrain <62119229+emrainemrain@users.noreply.github.com>
2020-05-13 15:59:06 +0100

    Update createChannel.java

--- src/day_1/createChannel.java
+++ src/day_1/createChannel.java
@@ -4,6 +4,13 @@ public class createChannel {
     public static void main(String[] args) {
 
         System.out.println("user is able to create channels");
+        
+        String s= "a";
+if(s.contains("a")){
+    System.out.println("contains");
+}else{
+    System.out.println("not");
+}
 
     }
 }
